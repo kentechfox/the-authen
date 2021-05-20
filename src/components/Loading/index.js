7bcf8f44bcb975a411576b1433601bd2adadb4fa
kenvandy <ken@techfox.io>
2021-05-20 14:20:28 +0700

    delete ticket

--- src/components/Loading/index.js
+++ src/components/Loading/index.js
@@ -8,8 +8,8 @@ function Loading(props) {
   const { isLoading } = props
   if (isLoading) {
     return (
-      <View style={styles.wrapper}>
-        <ActivityIndicator color={Colors.grayPrimary_1} size={'large'}/>
+      <View style={styles.wrapperss}>
+        <ActivityIndicator color={Colors.grayPrimary_1} size={'large'} />
       </View>
     )
   }
@@ -21,4 +21,3 @@ Loading.propTypes = {
 }
 
 export default Loading
-
